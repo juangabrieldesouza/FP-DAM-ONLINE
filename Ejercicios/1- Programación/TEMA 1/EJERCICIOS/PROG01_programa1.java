ebb77964fd3a05469849de485c3f9a75e7936e90
JuanGabriel <jgstudent7@hotmail.com>
2024-10-06 19:59:34 +0200

    TEMA 1 CONCLUIDO

---
+++ Ejercicios/1- Programación/TEMA 1/EJERCICIOS/PROG01_programa1.java
@@ -0,0 +1,6 @@
+class PROG01_programa1 {
+	public static void main(String[] args){
+		System.out.println("Módulo Profesional - PROGRAMACIÓN. UNIDAD DE TRABAJO 01\nIntroducción a la programación\nJuan Gabriel de Souza\nBurlada - NAVARRA\n10/05/2024\nPrograma1");
+	}
+
+}
